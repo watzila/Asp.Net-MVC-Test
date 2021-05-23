179ebe0f4979d0458b85d9c9098ff1692e740f84
建淞 王 <a2792839a@gmail.com>
2021-05-23 16:53:42 +0800

    新增base64用法

--- MVC各種練習/Controllers/QRCodeController.cs
+++ MVC各種練習/Controllers/QRCodeController.cs
@@ -9,9 +9,11 @@ namespace MVC各種練習.Controllers {
 	public class QRCodeController : Controller {
 
 		public ActionResult Index( ) {
+			ViewBag.img= CreateQRCode2( );
 			return View( );
 		}
 
+		//創建QRCode
 		public void CreateQRCode( ) {
 			System.IO.MemoryStream ms = new System.IO.MemoryStream( );//記憶流
 
@@ -34,7 +36,28 @@ namespace MVC各種練習.Controllers {
 			Response.BinaryWrite(ms.GetBuffer( ));//將二進位字元的字串寫入 HTTP 輸出資料流
 			//Response.Flush( );//緩衝區所有資料發送到客戶端
 			Response.End( );//緩衝區所有資料發送到客戶端並關閉
+		}
 
+		public static string CreateQRCode2( ) {
+			System.IO.MemoryStream ms = new System.IO.MemoryStream( );//記憶流
+
+			//創建QRCode
+			BarcodeWriter writer = new BarcodeWriter {
+				Format = BarcodeFormat.QR_CODE,//條碼類型
+				Options = new ZXing.QrCode.QrCodeEncodingOptions {
+					Width = 500,//寬
+					Height = 500,//高
+					CharacterSet = "UTF-8"
+				}
+			};
+
+			System.Drawing.Bitmap bitmap = writer.Write("123");//QRCode寫入字串
+
+			bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);//點陣圖以jpeg格式寫入到記憶流裡
+
+			byte[ ] arr = ms.ToArray();
+
+			return Convert.ToBase64String(arr);
 		}
 
 		public void aa( ) {
